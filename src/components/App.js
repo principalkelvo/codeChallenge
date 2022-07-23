54698bab998d7abbf3fea517f27d875bfba175f3
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-24 01:56:17 +0300

    fetch poems and add to state send items as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 function App() {
   const [poemForm, setPoemForm] = useState(false)
+  const [poems, setPoems] = useState([]);
+  useEffect(() => {
+    console.log("useEffect");
+    fetch("http://localhost:8004/poems")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log("setState");
+        setPoems(data);
+      });
+  }, []);
 
   function handleClick(e) {
     setPoemForm((poemForm) => !poemForm);
     console.log("handleClick: ")
   }
+  function onAddPoem(newPoem) {
+    setPoems([ ...poems,newPoem]);
+    console.log("new poems :", newPoem);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <button onClick={handleClick} >Show/hide new poem form</button>
-        {poemForm ? <NewPoemForm /> : null}
+        {poemForm ? <NewPoemForm onAddPoem={onAddPoem}/> : null}
       </div>
-      <PoemsContainer />
+      <PoemsContainer poems={poems}/>
     </div>
   );
 }
