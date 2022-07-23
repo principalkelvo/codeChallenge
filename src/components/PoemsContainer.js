6323e5a0c30b38c155f1d75a7ff77da41053a34c
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-24 01:58:54 +0300

    only passes props

--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Poem from "./Poem";
 
-function PoemsContainer() {
+function PoemsContainer({poems}) {
   return (
     <div className="poems-container">
       {/* render a list of <Poem> components in here */}
+      <Poem poems={poems}/>
     </div>
   );
 }
