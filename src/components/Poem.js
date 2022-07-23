173be5f5fd8029d8f39f857a4c2667bd564129e1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-24 01:58:39 +0300

    gets poems via props handles read and unread poems

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Poem() {
-  return (
-    <div>
-      <h3>Title</h3>
-      <p>Content</p>
+function Poem({poems}) {
+  const [read, setRead] = useState(true);
+
+  
+
+  function handleRead(){
+    setRead((read)=> !read)
+    console.log("handleRead: ");
+  }
+
+  console.log(poems);
+  const eachPoem = poems.map((poem) => (
+    <div key={poem.id}>
+      <h3>{poem.title}</h3>
+      <p>{poem.content}</p>
       <p>
-        <strong>- By Author</strong>
+        <strong>- {poem.author}</strong>
       </p>
-      <button>Mark as read</button>
+      <button onClick={handleRead}>{read ? 'Mark as read' : 'Mark as unread'}</button>
     </div>
-  );
+  ));
+  // console.log(eachPoem);
+  return <div>{eachPoem}</div>
+
+  // <div>
+  //   <h3>{eachPoem.title}</h3>
+  //   <p>Content</p>
+  //   <p>
+  //     <strong>- By Author</strong>
+  //   </p>
+  //   <button>Mark as read</button>
+  // </div>
 }
 
 export default Poem;
