5055daa3f6732915f9487ee494aec8294d6a6c2d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-24 01:57:44 +0300

    adds poems to server sends poem to app.js

--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPoemForm() {
+function NewPoemForm({ onAddPoem }) {
+  const [newPoems, setPoemForm] = useState({
+    title: "",
+    content: "",
+    author: "",
+  });
+  function handleNewPoems(event) {
+    setPoemForm({
+      ...newPoems,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(newPoems);
+    fetch("http://localhost:8004/poems", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newPoems.title,
+        content: newPoems.content,
+        author: newPoems.author,
+      }),
+    })
+      .then((r) => r.json())
+      .then((newPoem) => onAddPoem(newPoem));
+    const submittedFormData = {
+      title: "",
+      content: "",
+      author: "",
+    };
+    setPoemForm(submittedFormData);
+    console.log("submitted poem", newPoems);
+    console.log("cleared newPoems", submittedFormData);
+
+    console.log("submited");
+  }
   return (
-    <form className="new-poem-form">
-      <input placeholder="Title" />
-      <input placeholder="Author" />
-      <textarea placeholder="Write your masterpiece here..." rows={10} />
+    <form className="new-poem-form" onSubmit={handleSubmit}>
+      <input
+        value={newPoems.title}
+        onChange={handleNewPoems}
+        name="title"
+        placeholder="Title"
+      />
+      <input
+        value={newPoems.author}
+        onChange={handleNewPoems}
+        name="author"
+        placeholder="Author"
+      />
+      <textarea
+        value={newPoems.content}
+        onChange={handleNewPoems}
+        name="content"
+        placeholder="Write your masterpiece here..."
+        rows={10}
+      />
       <input type="submit" value="Share your masterpiece" />
     </form>
   );
